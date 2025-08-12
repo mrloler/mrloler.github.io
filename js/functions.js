7c1d67019c55648f8eb82994c423d61d8a859b1a
mrloler <calvinly123@gmail.com>
2025-08-12 02:57:38 +0200

    Fixed scroll to anchor
    getElementById("Anchor") not getElementById("#anchor")

--- js/functions.js
+++ js/functions.js
@@ -14,7 +14,7 @@ export function changePage(page, pushState = true)
         html_string = "/pages/404_page.html"; // Change to 404 page
     }
 
-    loadPage(html_string, document.getElementById("main"));
+    loadPage(html_string, document.getElementById("main"), anchor);
 
     // Add page to history/url
     if (pushState) {
@@ -22,19 +22,17 @@ export function changePage(page, pushState = true)
     }
 }
 
-export function loadPage(url, element) {
+export function loadPage(url, element, anchor) {
     fetch(url).then(response => response.text()).then(data => {
         element.innerHTML = data;
+        scrollToAnchor(anchor); // Scroll to anchor after loading
     })
-    
 }
 
 function scrollToAnchor(anchor) {
-    alert("!!!");
-    if (anchor.includes('#')){ // checks if anchor exists
-        const element = document.getElementById(anchor);
-        alert(element)
-        alert(anchor)
+    if (anchor && anchor.includes('#')) { // checks if anchor exists
+        const anchorId = anchor.split('#')[1]; // get part after #
+        const element = document.getElementById(anchorId);
         element.scrollIntoView({ behavior: 'smooth' });
     }
 }
